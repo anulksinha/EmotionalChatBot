595a5842d73ceebee29bbc99454bafac8b97e2fd
anul kumar sinha <anul kumar sinha>
2017-05-11 19:17:22 -0400

    reinforce

--- word2vec_utils.py
+++ word2vec_utils.py
@@ -18,7 +18,7 @@ def initialize():
     try:
         w2v_model
     except:
-        w2v_model = Word2Vec.load('movie_trained_w2v_model')#gensim.models.KeyedVectors.load_word2vec_format(word2vec_path, binary=True)#
+        w2v_model = gensim.models.KeyedVectors.load_word2vec_format(word2vec_path, binary=True)#Word2Vec.load('movie_trained_w2v_model')
     return( w2v_model ) 
 
 def get_unknown_vectors():
@@ -101,7 +101,7 @@ def split_sentence( sentence ):
     return sentence 
 
 def vectorize( sentence, pad_length = -1, model = None ):
-    global unknown_vectors 
+    global unknown_vectors
     global w2v_model
 
     if model is None:
