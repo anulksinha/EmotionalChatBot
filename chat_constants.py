b2a932c8673949485401fa8757b478b27ccb0027
paperspace <anthonyaag@gmail.com>
2017-05-11 21:34:34 -0400

    add demo file for one model which is very verbose due to null_punisher

--- chat_constants.py
+++ chat_constants.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-BLANK = np.array( [0]*299 + [1] )
+BLANK = np.array( [0]*300 + [1] )
 SPLIT_STRING = '+++$+++'
 EMOTE_DELIMITER = '@'
 BLANK_LINE = " ".join( [ '1', '9', '1', EMOTE_DELIMITER, '' ] )
 MIN_SENT_LENGTH = 10
 MAX_SENT_LENGTH = 30
-EMBED_DIM = 299#len(BLANK)
+EMBED_DIM = len(BLANK)
 VOCAB_SIZE = 10000
 UNK = VOCAB_SIZE + 1
 NULL = VOCAB_SIZE + 2
